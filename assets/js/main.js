cc911b4bb9026d397f5251c6e1d99e8d9e89a41d
Rafael Peralta Jr <rperaltajr@gmail.com>
2020-01-04 03:20:32 -0500

    Add location search and begin setting up for pagination feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,17 @@
-var vm = new Vue({
+const vm = new Vue({
     el: '#root',
     data: {       
         jobs: [],
         categories: [],
+        locations: [],
         keyword: '',
-        category: ''
+        category: '',
+        location: ''
     },
     
     computed: {
     	filteredByAll() {
-      	return getByCategory(getByKeyword(this.jobs, this.keyword), this.category)
+      	return getByLocation(getByCategory(getByKeyword(this.jobs, this.keyword), this.category), this.location)
       },
       // filteredByKeyword() {
 	    //   return getByKeyword(this.jobs, this.keyword)
@@ -32,6 +34,15 @@ var vm = new Vue({
               this.categories.push(job.category);
             }
           });
+
+          // add job locations to array
+          this.jobs.map((job) => {
+            // add location if it doesn't exist in array
+            if(this.locations.indexOf(job.location) === -1) {
+              this.locations.push(job.location);
+            }
+          });
+
         })
     }
 });
@@ -39,10 +50,16 @@ var vm = new Vue({
 function getByKeyword(list, keyword) {
   const search = keyword.trim().toLowerCase()
   if (!search.length) return list
-  return list.filter(item => item.title.toLowerCase().indexOf(search) > -1)
+  let hasNumber = /\d/;
+  return list.filter(item => item.title.toLowerCase().indexOf(search) > -1 || item.id == search)
 }
 
 function getByCategory(list, category) {
   if (!category) return list
   return list.filter(item => item.category === category)
+}
+
+function getByLocation(list, location) {
+  if (!location) return list
+  return list.filter(item => item.location === location)
 }
